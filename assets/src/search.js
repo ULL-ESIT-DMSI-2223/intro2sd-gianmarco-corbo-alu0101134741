3403d38e221d4f2eda5bfdb72b00bfcab4baa8c6
Gia-ui <72807522+Gia-ui@users.noreply.github.com>
2022-12-07 12:21:39 +0000

    Actualizado search.js

--- assets/src/search.js
+++ assets/src/search.js
@@ -1,3 +1,23 @@
 class JekyllSearch {
+  constructor(dataSource, searchField, resultsList, siteURL) {
   
+  }
+
+  fetchedData() {
+    return fetch(this.dataSource, {mode: 'no-cors'})
+      .then(blob => blob.json())
+  }
+
+  async findResults() {
+
+  }
+
+  async displayResults() {
+
+  }
+
+  // https://stackoverflow.com/questions/43431550/async-await-class-constructor
+  init() {
+    
+  }
 }
