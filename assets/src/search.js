06070dea0cf0532d257e2245ad1d8d3700fd8fec
Gia-ui <72807522+Gia-ui@users.noreply.github.com>
2022-12-28 21:36:59 +0000

    Updates

--- assets/src/search.js
+++ assets/src/search.js
@@ -1,9 +1,9 @@
 class JekyllSearch {
-  constructor(searchArguments) {
-    this.dataSource = searchArguments.dataSource
-    this.searchField = document.querySelector(searchArguments.searchField)
-    this.resultsList = document.querySelector(searchArguments.resultsList)
-    this.siteURL = searchArguments.siteURL
+  constructor(searchParameters) {
+    this.dataSource = searchParameters.dataSource
+    this.searchField = document.querySelector(searchParameters.searchField)
+    this.resultsList = document.querySelector(searchParameters.resultsList)
+    this.siteURL = searchParameters.siteURL
 
     this.data = [];
   }
